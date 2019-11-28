9bd837bbdf1561784c7dca137c2bcd4971903255
Sandy Pranjic <sandypranjic@gmail.com>
2019-11-28 10:51:07 -0500

    Updates

---
+++ precache-manifest.1b04fe6c7b80454c22f6f6d82ff8df5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ae14639d959748f235614c2dd17389",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "7a6010d7b78ef8f38a82",
+    "url": "/gh-pages-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8528975637c2ba6e5156",
+    "url": "/gh-pages-app/static/js/2.f26115e5.chunk.js"
+  },
+  {
+    "revision": "7a6010d7b78ef8f38a82",
+    "url": "/gh-pages-app/static/js/main.597d2268.chunk.js"
+  },
+  {
+    "revision": "e165b08d794ffde0ad3d",
+    "url": "/gh-pages-app/static/js/runtime-main.2c74c748.js"
+  }
+]);
